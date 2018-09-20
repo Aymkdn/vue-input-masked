54bff9f0ef720d2e2e9725b5511cfda157f49426
Aymeric <aymeric@kodono.info>
2018-09-20 16:10:10 +0200

    v0.1.0

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # vue-input-masked
-Yet another Vue component for input masking
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
---
+++ src/Demo.vue
@@ -0,0 +1,108 @@
+<template>
+  <div id="app">
+    <section class="page-header" id="header">
+      <h1 class="project-name">vue-input-masked</h1>
+      <h2 class="project-tagline">Simple masked input component for Vue.js 2.</h2>
+      <a href="https://github.com/Aymkdn/vue-input-masked" class="btn project-url">View on GitHub</a>
+    </section>
+    <section class="main-content">
+      <p>Same as <a href="https://github.com/niksmr/vue-masked-input">https://github.com/niksmr/vue-masked-input</a> but with the placeholder that stays visible while you're typing (see <a href="https://github.com/niksmr/vue-masked-input/issues/49">issue #49</a>).</p>
+      <hr>
+      <h3>Patterns:</h3>
+      <ul>
+        <li>1 – number</li>
+        <li>a – letter</li>
+        <li>A – letter, forced to upper case when entered</li>
+        <li>* – alphanumeric</li>
+        <li># – alphanumeric, forced to upper case when entered</li>
+        <li>+ – any character</li>
+      </ul>
+
+      <p>Use an <b>array</b> for <code>placeholder-char</code> to keep the placeholder up while typing.</p>
+      <p>Use a <b>string</b> for <code>placeholder-char</code> to get the normal behavior.</p>
+
+      <h3>Date: </h3>
+      <p>
+        <input-masked
+         type="text"
+         mask="1111-11-11"
+         placeholder="YYYY-MM-DD"
+         :placeholder-char="'YYYY-MM-DD'.split('')"
+         v-model="date"></input-masked> <span class="value">{{ date }}</span>
+       <h4>Code:</h4>
+       <pre>
+        <code>
+&lt;input-masked
+ type="text"
+ mask="1111-11-11"
+ placeholder="YYYY-MM-DD"
+ :placeholder-char="'YYYY-MM-DD'.split('')"
+ v-model="date"&gt;&lt;/input-masked&gt;
+        </code>
+       </pre>
+      </p>
+
+      <h3>Time: </h3>
+      <p>
+        <input-masked
+         type="text"
+         mask="11:11"
+         placeholder="hh:ss"
+         :placeholder-char="['h','h',':','s','s']"
+         v-model="time"></input-masked> <span class="value">{{ time }}</span>
+        <h4>Code:</h4>
+        <pre>
+         <code>
+&lt;input-masked
+ type="text"
+ mask="11:11"
+ placeholder="hh:ss"
+ :placeholder-char="['h','h',':','s','s']"
+ v-model="time"&gt;&lt;/input-masked&gt;
+         </code>
+        </pre>
+      </p>
+
+      <h3>Phone: </h3>
+      <p>
+        <input-masked
+         type="text"
+         mask="\+11 (111) 1111-11"
+         placeholder="+11 (111) 1111-11"
+         placeholder-char="_"
+         v-model="phone"></input-masked> <span class="value">{{ phone }}</span>
+        <h4>Code:</h4>
+        <pre>
+         <code>
+&lt;input-masked
+ type="text"
+ mask="\+11 (111) 1111-11"
+ placeholder="+11 (111) 1111-11"
+ placeholder-char="_"
+ v-model="phone"&gt;&lt;/input-masked&gt;
+         </code>
+        </pre>
+      </p>
+    </section>
+  </div>
+</template>
+
+<script>
+import InputMasked from './vue-input-masked.js'
+
+export default {
+  name: 'app',
+  components: {
+    InputMasked
+  },
+  data () {
+    return {
+      date:'',
+      time:'',
+      phone:''
+    }
+  }
+}
+</script>
+
+<style src="./jekyll.css"></style>
